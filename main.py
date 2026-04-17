7812831c91cad6e54b3585c49b87146a452415a0
Illevagudo225 <70183736+Illevagudo225@users.noreply.github.com>
2026-04-17 18:28:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from pyscript import display
 
-
-# variables
-x = np.linspace(0, 10, 100)
-y = np.sin(x)
-
-# the whole graph code
-fig, ax = plt.subplots(figsize=(7, 4))
-ax.plot(x, y, color='green', label='Graph yes')
-ax.set_title("Nice")
-ax.grid(True)
-
-# display it to the html
-display(fig, target="graph-output")
